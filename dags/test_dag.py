914993d0ed1ec8eaff0fe7889e3a9fb7ef5db7f1
PavelShestakov <pavel.shestakov98@yandex.ru>
2026-03-19 17:33:52 +0300

    first com

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -5,13 +5,18 @@ from datetime import datetime
 def hello():
     print("Hello from Airflow in Kubernetes")
 
+
 with DAG(
     dag_id="hello_world",
     start_date=datetime(2026, 3, 1),
     schedule=None,
     catchup=False,
 ) as dag:
+
     t1 = PythonOperator(
         task_id="say_hello",
         python_callable=hello,
     )
+
+
+    t1
